2ff4798ecf910ad35222a1938e3e81d0ccde761d
Marcio Marques Rosa <marcio.mmrs@gmail.com>
2020-10-15 15:07:13 -0300

    Terceiro dia da semana nlw

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { FiArrowRight } from 'react-icons/fi';
 
 import '../styles/global.css';
-import '../styles/page/landing.css';
+import '../styles/pages/landing.css';
 
 import logoImg from '../images/logo.svg';
 
